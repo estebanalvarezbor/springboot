3e4e7ac0582bc57501bd3f7a2e93f56ef87ab43a
estebanalvarezbor <esteban.alvarez.bor@gmail.com>
2024-06-17 22:53:21 -0600

    Ejemplo y curso

---
+++ Curso/SB-servicio-productos/src/main/java/com/springboot/app/productos/models/service/ProductoServiveImpl.java
@@ -0,0 +1,29 @@
+package com.springboot.app.productos.models.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.app.productos.models.dao.ProductoDao;
+import com.springboot.app.productos.models.entity.Producto;
+
+@Service
+public class ProductoServiveImpl implements IProductoService{
+	
+	@Autowired
+	private ProductoDao productoDao;
+
+	@Override
+	@Transactional(readOnly=true)
+	public List<Producto> encontrarTodos() {
+		return (List<Producto>) productoDao.findAll();
+	}
+
+	@Override
+	public Producto encontarPorId(long id) {
+		return (Producto) productoDao.findById(id).orElse(null);
+	}
+
+}
