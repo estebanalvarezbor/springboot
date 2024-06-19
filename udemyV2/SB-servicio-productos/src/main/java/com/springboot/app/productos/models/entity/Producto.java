9261ab499e6f85e79da15d9fff47a1b22ac986ce
estebanalvarezbor <esteban.alvarez.bor@gmail.com>
2024-06-18 23:15:36 -0600

    microservicios restTemplate udemy

---
+++ udemyV2/SB-servicio-productos/src/main/java/com/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,58 @@
+package com.springboot.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="productos")
+public class Producto implements Serializable {
+
+	private static final long serialVersionUID = 5473211889534357916L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	private Double precio;
+	
+	@Column(name="creado_en")
+	@Temporal(TemporalType.DATE)
+	private Date creadoEn;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreadoEn() {
+		return creadoEn;
+	}
+	public void setCreadoEn(Date creadoEn) {
+		this.creadoEn = creadoEn;
+	}
+
+
+}
---
+++ udemyV2/SB-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO productos (nombre, precio, creado_en) VALUES('Sony', 800, NOW());
+INSERT INTO productos (nombre, precio, creado_en) VALUES('Samsung', 900, NOW());
+INSERT INTO productos (nombre, precio, creado_en) VALUES('LG', 850, NOW());
+INSERT INTO productos (nombre, precio, creado_en) VALUES('Toshiba', 420, NOW());
+INSERT INTO productos (nombre, precio, creado_en) VALUES('Philips', 3120, NOW());
